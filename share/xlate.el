0aeca1b2fba3a12c6c8a594c8dc7beeb61eb6331
Kazumasa Utashiro <kaz@utashiro.com>
2023-03-20 14:48:11 +0900

    asked ChatGPT to modify the function to accept language as an optional argument

--- share/xlate.el
+++ share/xlate.el
@@ -7,14 +7,19 @@
       (xlate-region (region-beginning) (region-end))
     (xlate-region (point-min) (point-max))))
 
-(defun xlate-region (begin end)
-  "Execute greple -Mxlate on region."
-  (interactive "r")
-  (let ((opoint (point)))
+(defun xlate-region (begin end &optional target-lang)
+  "Execute xlate on region with an optional target language.
+If called with a prefix argument (C-u), prompt for the target language."
+  (interactive (let* ((region (list (region-beginning) (region-end)))
+                      (lang (when current-prefix-arg
+                              (read-string "Target language (default: EN-US): "))))
+                 (append region (list lang))))
+  (let ((opoint (point))
+        (lang (or target-lang "EN-US")))
     (set-mark end)
     (goto-char begin)
     (shell-command-on-region
      begin end
-     "xlate -a -s -o cm -w72 -p '(?s).+' -t EN-US"
+     (format "xlate -a -s -o cm -w72 -p '(?s).+' -t %s -" lang)
      t t nil t)
     (goto-char (region-beginning))))
