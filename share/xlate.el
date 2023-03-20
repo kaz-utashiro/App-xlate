fffe454ebca37250db56ad7a672d089c6fc9ac2f
Kazumasa Utashiro <kaz@utashiro.com>
2023-03-20 19:25:41 +0900

    asked ChatGPT to remember the entered language as a default for next

--- share/xlate.el
+++ share/xlate.el
@@ -1,5 +1,8 @@
 ;;; xlate.el
 
+(defvar xlate-default-target-lang "EN-US"
+  "Default target language for translation.")
+
 (defun xlate-buffer (&optional prefix)
   "Execute deepl command on current buffer."
   (interactive "P")
@@ -12,14 +15,16 @@
 If called with a prefix argument (C-u), prompt for the target language."
   (interactive (let* ((region (list (region-beginning) (region-end)))
                       (lang (when current-prefix-arg
-                              (read-string "Target language (default: EN-US): "))))
+                              (read-string (format "Target language (default: %s): "
+						   xlate-default-target-lang)))))
                  (append region (list lang))))
   (let ((opoint (point))
-        (lang (or target-lang "EN-US")))
+        (lang (or target-lang xlate-default-target-lang)))
     (set-mark end)
     (goto-char begin)
     (shell-command-on-region
      begin end
      (format "xlate -a -s -o cm -w72 -p '(?s).+' -t %s -" lang)
      t t nil t)
-    (goto-char (region-beginning))))
+    (goto-char (region-beginning))
+    (setq xlate-default-target-lang lang))) ; Update the default target language
